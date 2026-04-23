674965825aee7bde94da40991b4628e3a9ad59ae
Rohit Thete <rohitthete.512@gmail.com>
2026-04-23 17:38:55 +0530

    Created Models

---
+++ api/models.py
@@ -0,0 +1,57 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=10,unique=True)
+
+    REQUIRED_FIELDS =["email","phone"]
+
+    class Meta:
+        ordering=["username"]
+
+    def __str__(self):
+        return f"{self.username} - {self.email}"
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=20,unique=True) 
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=20)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    stock = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=10,decimal_places=2)
+
+    
+    def __str__(self):
+        return f"{self.name} - {self.category} - {self.stock} - {self.price}"
+    
+
+class Order(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product,through='OrderItem')
+    date_created = models.DateTimeField(auto_now_add=True)
+    total_bill = models.DecimalField(max_digits=10,decimal_places=2)
+
+    def __str__(self):
+        return f"{self.user} - {self.date_created}"
+    
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order,on_delete=models.CASCADE)
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=10,decimal_places=2)
+
+    def __str__(self):
+        return f"{self.order} - {self.product} - {self.quantity} - {self.price}"
+
+
+
+
