1313e11eda1622591f877b4fa7120f1453d5ec61
Rohit Thete <rohitthete.512@gmail.com>
2026-04-27 11:09:06 +0530

    Made minor changes to models

--- api/models.py
+++ api/models.py
@@ -4,6 +4,8 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class User(AbstractUser):
+    first_name = None
+    last_name = None
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=10,unique=True)
 
