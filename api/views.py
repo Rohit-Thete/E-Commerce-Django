5fabe4b37e0f2c00031fd85e0d350737639fcbd2
Rohit Thete <rohitthete.512@gmail.com>
2026-04-27 11:06:26 +0530

    Added Register Api for user

--- api/views.py
+++ api/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from .models import User
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .serializers import RegisterSerializer
 
 # Create your views here.
+@api_view(["POST"])
+def register(request):
+    data = request.data
+    serializer = RegisterSerializer(data = data)
+    if serializer.is_valid():
+        user = serializer.save()
+        return Response({"msg":"User Created","user":{user.username}},status=201)
+    
+    return Response(serializer.errors,status=400)
+
+
+
+# class UserView(APIView):
+#     def get()       
+
+
