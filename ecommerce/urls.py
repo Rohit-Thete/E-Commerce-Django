5f1b14c4431a7232906e479e0330be3cd3907d86
Rohit Thete <rohitthete.512@gmail.com>
2026-05-12 14:05:26 +0530

    changes in urls file

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -17,10 +17,13 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
-    path("silk/", include("silk.urls", namespace="silk")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += [path("silk/", include("silk.urls", namespace="silk"))]
 
