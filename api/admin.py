91a3b249fe5510625208dbf20f34f5823de1d2a6
Rohit Thete <rohitthete.512@gmail.com>
2026-05-18 20:46:06 +0530

    Added an abstract model

--- api/admin.py
+++ api/admin.py
@@ -5,24 +5,24 @@ from .models import User, Category, Product, Order, OrderItem
 # Register your models here.
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ["username", "role", "email", "phone"]
+    list_display = ["id", "username", "role", "email", "phone","created_at"]
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ["name"]
+    list_display = ["id","name","created_at","is_active"]
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ["name", "price", "category", "stock"]
+    list_display = ["id", "name", "price", "category", "stock","created_at","is_active"]
 
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ["user", "status", "total_bill", "date_created"]
+    list_display = ["id", "user", "status", "total_bill", "created_at"]
 
 
 @admin.register(OrderItem)
 class OrderItemAdmin(admin.ModelAdmin):
-    list_display = ["order", "product", "quantity", "price"]
+    list_display = ["id", "order", "product", "quantity", "price","created_at","is_active"]
