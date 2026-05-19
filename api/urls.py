c88e4d9fa8f1b41648c2134215271a69c6368672
Rohit Thete <rohitthete.512@gmail.com>
2026-05-19 11:54:44 +0530

    Added model viewset to category

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,17 @@
 from .views import register, login_user, UserView, CategoryView, ProductView, OrderView
-from django.urls import path
+from django.urls import path,include
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register("category", CategoryView, basename="category")
 
 urlpatterns = [
     path("register/", register),
     path("login/", login_user),
     path("user/", UserView.as_view()),
-    path("category/", CategoryView.as_view()),
-    path("category/<int:pk>/", CategoryView.as_view()),
+    path("category/", include(router.urls)),
+    # path("category/", CategoryView.as_view()),
+    # path("category/<int:pk>/", CategoryView.as_view()),
     path("product/", ProductView.as_view()),
     path("product/<int:pk>/", ProductView.as_view()),
     path("order/", OrderView.as_view()),
